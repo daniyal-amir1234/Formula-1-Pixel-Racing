f23beea912a0730dd9f9e6967b00b6a810d00fb0
Daniyal Amir <daniyalamir2006@gmail.com>
2026-01-06 22:26:54 +0000

    Add Formula 1 Pixel Racing game description
    Initial commit of Formula 1 Pixel Racing game.

---
+++ formula1v78.py
@@ -0,0 +1,15 @@
+"""
+Formula 1 Pixel Racing (Beta)
+
+Author: Daniyal Amir
+Started: June 2024
+Language: Python
+Framework: Pygame
+
+A retro-inspired top-down Formula 1 time trial game.
+Built entirely in a single file as a solo project, with
+custom rendering, physics, camera, and tyre wear systems.
+
+Originally developed as an OCR A-Level CS NEA (69/70),
+then extended post-submission.
+"""
